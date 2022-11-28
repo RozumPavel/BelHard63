fdf55adf1b24fd1579dab75d5450ba631cfb308c
RozumPavel <pasha_rozum@mail.ru>
2022-11-28 23:40:36 +0300

    second_task_finish

---
+++ Lesson№3/second_task.py
@@ -0,0 +1,8 @@
+x = float(input("Введи первое число: "))
+y = float(input("Введи второе число: "))
+z = float(input("Введи третье число: "))
+three_sums = (x, y, z)
+general_sum = sum(three_sums)
+pre_answer = general_sum/3
+answer = round(pre_answer, 3)
+print(answer)
