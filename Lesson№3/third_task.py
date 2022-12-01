b5cce07e7def5d6d00a20492b2f2452e21fc4bc7
RozumPavel <pasha_rozum@mail.ru>
2022-12-01 19:49:45 +0300

    finish with the second way (task 3), also rename variables

--- Lesson№3/third_task.py
+++ Lesson№3/third_task.py
@@ -1,7 +1,16 @@
 # Пользователь вводит Имя, Возраст и Город, сформировать приветственное сообщение путем форматирования 3-мя способами
 # Сделал 4 (конкатенация, %форматирование, format, f_string)
-response = input("Введите ваше имя: ")
-response1 = input("Введите ваш возраст: ")
-response2 = input("Введите ваш город: ")
-answer = response + ' ' + response1 + ' ' + response2
+name = input("Введите ваше имя: ")
+age = input("Введите ваш возраст: ")
+city = input("Введите ваш город: ")
+answer = name + ' ' + age + ' ' + city
 print("Приветствую," + ' ' + answer)
+
+name1 = input("Введите ваше имя: ")
+age1 = input("Введите ваш возраст: ")
+city1 = input("Введите ваш город: ")
+name_feedback = name1
+age_feedback = age1
+city_feedback = city1
+answer2 = f"Приветствую, {name_feedback} {age_feedback} {city_feedback}"
+print(answer2)
