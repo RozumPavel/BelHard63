ae76c861a0cf7600b907694e693e53917d65315e
RozumPavel <pasha_rozum@mail.ru>
2022-12-01 20:16:00 +0300

    finish with the fourth way (third task)

--- Lesson№3/third_task.py
+++ Lesson№3/third_task.py
@@ -6,9 +6,14 @@ city = input("Введите ваш город: ")
 concat_answer = name + ' ' + age + ' ' + city
 print("Приветствую," + ' ' + concat_answer)
 
+
 f_string_answer = f"Приветствую, {name} {age} {city}"
 print(f_string_answer)
+
 
 int_age = int(age)
 percent_formatting_answer = "Приветствую, %s %d %s" % (name, int_age, city)
 print(percent_formatting_answer)
+
+format_formatting_answer = "Приветствую, {} {} {}".format(name, age, city)
+print(format_formatting_answer)
