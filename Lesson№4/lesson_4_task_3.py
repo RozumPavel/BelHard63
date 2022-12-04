3c75beb18401a97de49f2eaa55faa5a466b59329
RozumPavel <pasha_rozum@mail.ru>
2022-12-04 20:05:37 +0300

    optimization

--- Lesson№4/lesson_4_task_3.py
+++ Lesson№4/lesson_4_task_3.py
@@ -2,15 +2,10 @@
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
 
-user_name = input("Введите ваше имя: ")
-user_email = input("Введите вашу почту: ")
-user_name1 = input("Введите ваше имя: ")
-user_email1 = input("Введите вашу почту: ")
-
-dict1 = {"name": user_name, "email": user_email}
+dict1 = {"name": input("Введите ваше имя: "), "email": input("Введите вашу почту: ")}
 dict2 = {i: dict1 for i in "0"}
 
-dict3 = {"name": user_name1, "email": user_email1}
+dict3 = {"name": input("Введите ваше имя: "), "email": input("Введите вашу почту: ")}
 dict4 = {i: dict3 for i in "1"}
 
 general_dict = dict2 | dict4
