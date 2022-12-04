113b6656ec049c942d7bf940fa3a8aee570c1b48
RozumPavel <pasha_rozum@mail.ru>
2022-12-04 17:16:14 +0300

    task condition to the beginning

--- Lesson№4/lesson_4_task_1.py
+++ Lesson№4/lesson_4_task_1.py
@@ -1,3 +1,4 @@
+# Заполнить список степенями числа 2 (от 2^1 до 2^n).
 number_from_user = int(input("Введите степень до которой необходимо сделать рассчет: "))
 answer = [2**i for i in range(1, number_from_user + 1)]
 print(answer)
