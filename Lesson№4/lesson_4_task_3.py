f908d56f78bfdb1399eb459b8fd7a7106d4a2b18
RozumPavel <pasha_rozum@mail.ru>
2022-12-04 19:33:19 +0300

    finish with third task

---
+++ Lesson№4/lesson_4_task_3.py
@@ -0,0 +1,17 @@
+# Заполнить словарь где ключами будут выступать числа от 0 до n, а
+# значениями вложенный словарь с ключами "name" и "email", а значения
+# для этих ключей будут браться с клавиатуры
+
+user_name = input("Введите ваше имя: ")
+user_email = input("Введите вашу почту: ")
+user_name1 = input("Введите ваше имя: ")
+user_email1 = input("Введите вашу почту: ")
+
+dict1 = {"name": user_name, "email": user_email}
+dict2 = {i: dict1 for i in "1"}
+
+dict3 = {"name": user_name1, "email": user_email1}
+dict4 = {i: dict3 for i in "2"}
+
+general_dict = dict2 | dict4
+print(general_dict)
