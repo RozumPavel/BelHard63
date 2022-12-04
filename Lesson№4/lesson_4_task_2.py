dae34bcf7ea45d366222d1790da96f6dbef08606
RozumPavel <pasha_rozum@mail.ru>
2022-12-04 17:14:42 +0300

    new way

--- Lesson№4/lesson_4_task_2.py
+++ Lesson№4/lesson_4_task_2.py
@@ -1,3 +1,14 @@
+# Без использования collections, написать программу, которая будет
+# создавать словарь для подсчитывания количества вхождений каждой
+# буквы в текст введенный с клавиатуры
+
+# 1
 word_from_user = input("Введите ваш текст: ")
 answer = {i: word_from_user.count(i) for i in word_from_user}
+print(answer)
+
+# 2
+answer = {}
+for i in word_from_user:
+    answer[i] = word_from_user.count(i)
 print(answer)
