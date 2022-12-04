1b94e5de5266f3f5fd73eb6de27992e5720be3c8
RozumPavel <pasha_rozum@mail.ru>
2022-12-04 11:52:37 +0300

    finish with task 2

--- Lesson№4/lesson_4_task_2.py
+++ Lesson№4/lesson_4_task_2.py
@@ -1,3 +1,3 @@
 word_from_user = input("Введите ваш текст: ")
-answer = {symbol: word_from_user.count(symbol) for symbol in word_from_user}
+answer = {i: word_from_user.count(i) for i in word_from_user}
 print(answer)
