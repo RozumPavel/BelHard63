aa7ea941a9fa844755ceead875b2e95c3a874200
RozumPavel <pasha_rozum@mail.ru>
2022-12-05 19:05:46 +0300

    finish task

--- Lesson№4/additional_task.py
+++ Lesson№4/additional_task.py
@@ -1,12 +1,12 @@
 # Сгенерировать два списка рандомных чисел и посчитать количество общих элементов
 import random
 from random import randint
-
 random_numbers_first = [randint(0,50) for i in range(10)]
 random_numbers_second = [randint(-10,40) for i in range(10)]
 
 first_set = set(random_numbers_first)
 second_set = set(random_numbers_second)
 
-answer = first_set & second_set
+first_set_second_set = first_set & second_set
+answer = len(first_set_second_set)
 print(answer)
