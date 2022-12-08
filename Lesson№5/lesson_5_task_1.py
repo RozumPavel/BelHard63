1cbcd7cc55a1456ea883cadc6cdb933a243e3f46
RozumPavel <119065936+RozumPavel@users.noreply.github.com>
2022-12-08 16:03:44 +0300

    Update lesson_5_task_1.py

--- Lesson№5/lesson_5_task_1.py
+++ Lesson№5/lesson_5_task_1.py
@@ -1,3 +1,10 @@
 # Вывести первые N чисел кратные M и больше K
-numbers = [i for i in range(100+ 1) if not i % 9 and i > 20]
-print(numbers[:5])
+numbers = []
+while (len(numbers)<5):
+    number_from_user = input("Введите число: ")
+    while not number_from_user.isdigit():
+        number_from_user = input("Введите ваше число: ")
+    if not int(number_from_user) % 7 and int(number_from_user) > 10:
+        numbers.append(number_from_user)
+    print(numbers)
+      
