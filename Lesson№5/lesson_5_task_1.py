e0f5e59a623497ef345558f82c768494600e43d2
RozumPavel <119065936+RozumPavel@users.noreply.github.com>
2022-12-08 16:47:22 +0300

    Update lesson_5_task_1.py

--- Lesson№5/lesson_5_task_1.py
+++ Lesson№5/lesson_5_task_1.py
@@ -1,10 +1,12 @@
 # Вывести первые N чисел кратные M и больше K
+n = int(input("Введите сколько чисел вывести: "))
+m = int(input("Введите число которому должно быть кратно введенное вами: "))
+k = int(input("Введите число больше которого должны быть выведенные числа: "))
 numbers = []
-while (len(numbers)<5):
+while (len(numbers)<n):
     number_from_user = input("Введите ваше число: ")
-    while not number_from_user.isdigit():
-        number_from_user = input("Введите число: ")
-    if not int(number_from_user) % 7 and int(number_from_user) > 10:
+    if not number_from_user.isdigit():
+        number_from_user = input("Вы ввели не число.Введите число: ")
+    if not int(number_from_user) % m and int(number_from_user) > k:
         numbers.append(number_from_user)
-    print(numbers)
-      
+    print(numbers)      
