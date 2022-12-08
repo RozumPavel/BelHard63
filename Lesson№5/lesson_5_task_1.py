149577b1c0e1cd5e3959ad0d82e0e94844daf644
RozumPavel <119065936+RozumPavel@users.noreply.github.com>
2022-12-08 13:37:29 +0300

    Update lesson_5_task_1.py

--- Lesson№5/lesson_5_task_1.py
+++ Lesson№5/lesson_5_task_1.py
@@ -1,3 +1,3 @@
 # Вывести первые N чисел кратные M и больше K
-numbers = [i for i in range(100+ 1) if not i % 7 and i > 10]
+numbers = [i for i in range(100+ 1) if not i % 9 and i > 20]
 print(numbers[:5])
