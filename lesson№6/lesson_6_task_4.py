dfa3a5b1225bbaff783dbb89953dca0a706b6c10
RozumPavel <pasha_rozum@mail.ru>
2022-12-13 02:47:51 +0300

    finish task

--- lesson№6/lesson_6_task_4.py
+++ lesson№6/lesson_6_task_4.py
@@ -1,13 +1,9 @@
-# Дан список содержащий в себе различные типы данных, отфильтровать таким
-# образом, чтобы остались только строки, использование дополнительного списка
-# незаконно
 
-
-def without_digits(text):
+def delete_digits(text):
     for i in text:
         if not isinstance(i, str):
             text.remove(i)
 
-text = [123, "hellow"]
-without_digits(text)
+text = [123, "hellow", 123123, "cars"]
+delete_digits(text)
 print(text)
